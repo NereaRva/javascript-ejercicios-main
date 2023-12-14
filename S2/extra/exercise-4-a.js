3d9faf428982753d7c6ce1f5003cba45c149c1c9
NereaRVA <nerea.rva1@gmail.com>
2023-12-14 12:57:17 +0100

    realizo ejercicios extras

--- S2/extra/exercise-4-a.js
+++ S2/extra/exercise-4-a.js
@@ -1,13 +1,13 @@
 
 const palabras = ['Caracol', 'Mosquito', 'Salamandra', 'Ajolote']
-
 function findArrayIndex(array, text) {
     for (let i = 0; i < array.length; i++) {
-        if (array[i]==text) {
-            console.log(i)
-        }
-        
+      if (array[i] === text) {
+        return i;
+      }
     }
-}
-
-findArrayIndex(palabras,"Mosquito")
+    
+    return -1;
+  }
+  findArrayIndex(palabras,"Salamandra")
+console.log(findArrayIndex(palabras,"Salamandra"));
